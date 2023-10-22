b50b9c3c8f70ba5b436d205dbac6aae9cfbd01f8
matisantillan11 <matias1.santillan@gmail.com>
2023-10-22 02:12:19 +0000

    map added

--- src/components/map/map.component.tsx
+++ src/components/map/map.component.tsx
@@ -1,6 +1,16 @@
 import './map.styles.css'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 export const MapComponent = () => {
-    return <div className="map_container">
-
-    </div>
+    return (
+        <div className="map_container">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]} />
+            </MapContainer>
+        </div>
+    )
 }
